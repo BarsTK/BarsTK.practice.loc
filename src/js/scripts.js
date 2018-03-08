4fb2e728631a20d701ffaeaed6cb180e24f971e2
BarsTK <zinin_uladzimir@epam.com>
2018-03-08 07:21:13 +0300

    - add block footer with all elements and adaptivity; - add adaptivity to everywhere where not was; - shange little bit select with currency; - add button to move up of page; - add appropriate scripts

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,11 +1,36 @@
-$(function(){
-    $('.menu-toggle-btn').on('click', function(){
-        //$(this).next().slideToggle(600);
-        $(this).next().slideToggle(600, function(){
+$(function() {
+    $('.menu-toggle-btn').on('click', function() {
+        $(this).next().slideToggle(600, function() {
             const menu = $(this);
             if(menu.css('display') === 'none'){
                 menu.removeAttr('style');
             }
         });
     });
+});
+
+$(function() {
+    $(window).scroll(function() {
+        if($(this).scrollTop() !== 0) {
+            $('#toTop').fadeIn();
+        } else {
+            $('#toTop').fadeOut();
+        }
+    });
+    $('#toTop').click(function() {
+        $('body, html').animate({scrollTop:0}, 700);
+    });
+});
+
+$(function() {
+    $('.hider').click(function() {
+        const $elemBefore = $(this).parent().next();
+        if($elemBefore.css('display') !== 'none') {
+                $elemBefore.hide(700);
+            }
+            else {
+                $elemBefore.show(700);
+            }
+        }
+    );
 });
